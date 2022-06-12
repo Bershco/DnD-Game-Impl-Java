4943a1fc9436b3c6a297518f215170c9a1abfc11
Tom Dotan <tomdot@post.bgu.ac.il>
2022-06-12 19:30:20 +0300

    Added v1.0 of Warrior class

--- OOPPrinciples_Assignment3/src/Warrior.java
+++ OOPPrinciples_Assignment3/src/Warrior.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Warrior {
+public class Warrior extends Player {
+    private int abilityCooldown;
+    private int remainingCooldown;
+
+    public Warrior(int cooldown){
+        abilityCooldown = cooldown;
+        remainingCooldown = 0;
+    }
+
+    @Override
+    public void onLevelUp() {
+        super.onLevelUp();
+        remainingCooldown = 0;
+        healthPool += playerLevel * 5;
+        attackPoints += playerLevel * 2;
+        defensePoints += playerLevel;
+    }
+
+    @Override
+    public void onGameTick() {
+        super.onGameTick();
+
+        remainingCooldown = Math.max(0, remainingCooldown - 1);
+    }
+
+    @Override
+    protected boolean enoughResources() {
+        return remainingCooldown == 0;
+    }
+
+    @Override
+    public void castAbility() {
+        //TODO: provide error message
+        if (enoughResources()) {
+            remainingCooldown = abilityCooldown;
+            healthAmount = Math.min(healthPool, healthAmount + defensePoints * 10);
+            return;
+        }
+
+    }
 }
