26dcd649739461f4a4b61b38cd3e5ec52c824c67
Tom Dotan <tomdot@post.bgu.ac.il>
2022-06-12 19:43:38 +0300

    Changed access modifiers for the Player class

--- OOPPrinciples_Assignment3/src/Tile.java
+++ OOPPrinciples_Assignment3/src/Tile.java
@@ -1,11 +1,11 @@
 public class Tile {
-    public char Tile;
+    public char tile;
     public Position pos;
     public double range(Tile other) {
         return Math.sqrt(Math.pow((pos.x - other.pos.x),2)+Math.pow((pos.x - other.pos.x),2));
     }
 
     public String toString() {
-        return ""+Tile;
+        return ""+ tile;
     }
 }
