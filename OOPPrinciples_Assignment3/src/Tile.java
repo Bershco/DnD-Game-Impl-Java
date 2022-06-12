7ce94c0626ac121c25d22f0d31097a505958e5a1
Bershco <hersco@post.bgu.ac.il>
2022-06-12 18:12:01 +0300

    Implemented Tile and Position classes

---
+++ OOPPrinciples_Assignment3/src/Tile.java
@@ -0,0 +1,11 @@
+public class Tile {
+    public char Tile;
+    public Position pos;
+    public double range(Tile other) {
+        return Math.sqrt(Math.pow((pos.x - other.pos.x),2)+Math.pow((pos.x - other.pos.x),2));
+    }
+
+    public String toString() {
+        return ""+Tile;
+    }
+}
