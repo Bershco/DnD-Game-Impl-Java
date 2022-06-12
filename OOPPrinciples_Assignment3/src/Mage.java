1ca146179fd36db14717ac0e0ca110f1b09f26c9
Tom Dotan <tomdot@post.bgu.ac.il>
2022-06-12 19:30:44 +0300

    Added v1.0 of Mage class

--- OOPPrinciples_Assignment3/src/Mage.java
+++ OOPPrinciples_Assignment3/src/Mage.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Mage {
+public class Mage extends  Player{
+    private int manaPool;
+    private int currentMana;
+    private int manaCost;
+    private int spellPower;
+    private int hitCount;
+    private int abilityRange;
+
+    public Mage(int manaPool, int manaCost, int spellPower, int hitCount, int abilityRange) {
+        this.manaPool = manaPool;
+        currentMana = manaPool / 4;
+        this.manaCost = manaCost;
+        this.spellPower = spellPower;
+        this.hitCount = hitCount;
+        this.abilityRange = abilityRange;
+    }
+
+    @Override
+    public void onLevelUp() {
+        super.onLevelUp();
+
+        manaPool += playerLevel * 25;
+        currentMana = Math.min(manaPool, currentMana + manaPool / 4);
+        spellPower += playerLevel * 10;
+    }
+
+    @Override
+    public void onGameTick() {
+        super.onGameTick();
+
+        currentMana = Math.min(manaPool, currentMana + playerLevel);
+    }
+
+    @Override
+    protected boolean enoughResources() {
+        return currentMana >= manaCost;
+    }
+
+    @Override
+    public void castAbility() {
+        if(enoughResources()) {
+            currentMana -= manaCost;
+            int hits = 0;
+            while (hits < hitCount ) { //TODO: add living enemy exists within range clause
+                //TODO: damage an enemy within the ability range (it will try to defend itself)
+                hits++;
+            }
+        }
+
+        //TODO: provide error message
+    }
 }
