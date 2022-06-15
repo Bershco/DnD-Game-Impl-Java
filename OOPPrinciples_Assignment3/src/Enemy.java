651af406807293ea416d4358e3eab851a59b1a23
Bershco <hersco@post.bgu.ac.il>
2022-06-15 18:07:54 +0300

    Added gettter and setter to experienceValue, and added javadoc to all methods

--- OOPPrinciples_Assignment3/src/Enemy.java
+++ OOPPrinciples_Assignment3/src/Enemy.java
@@ -1,7 +1,20 @@
 public class Enemy extends Unit{
-    public int experienceValue;
 
+    private int experienceValue;
     Board b = Board.getInstance();
+
+    public int getExperienceValue() {
+        return experienceValue;
+    }
+
+    protected void setExperienceValue(int exp) {
+        experienceValue = exp;
+    }
+
+    /**
+     * This method is part of the combat system of the game, it is used to attack another unit (primarily the player)
+     * @param target The unit to attack
+     */
     @Override
     public void dealDamage(Unit target) {
         super.dealDamage(target);
@@ -10,11 +23,21 @@ public class Enemy extends Unit{
         }
     }
 
+    /**
+     * This method describes a death of an enemy
+     */
     public void death() {
         b.currentPosition[pos.x][pos.y] = new Empty();
         b.removeEnemy(this);
     }
-    
+
+    /**
+     * This method attempts movement of the enemy to a certain direction, followed by the following options:
+     *      1. If the attempted tile is empty, simply move
+     *      2. If the attempted tile is the player, attempt fighting sequence
+     *      3. If the attempted tile is a wall, just randomly move to a different direction
+     * @param d the attempted direction to move towards
+     */
     @Override
     public void move(Direction d) {
         Board b = Board.getInstance();
@@ -69,6 +92,10 @@ public class Enemy extends Unit{
             }
         }
     }
+
+    /**
+     * This method moves the enemy randomly using a simple Math.random method call
+     */
     public void randomizeMove() {
         int direction = Math.round((float)Math.random()*5) + 1;
         switch (direction) {
