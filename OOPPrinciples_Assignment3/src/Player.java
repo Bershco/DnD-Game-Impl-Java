192057128e8f96ae53f132f9c372c5f628219f6e
Bershco <hersco@post.bgu.ac.il>
2022-06-15 18:12:01 +0300

    Implemented death() and added javadoc

--- OOPPrinciples_Assignment3/src/Player.java
+++ OOPPrinciples_Assignment3/src/Player.java
@@ -26,18 +26,24 @@ public class Player extends Unit implements HeroicUnit{
             onLevelUp();
     }
 
+    /**
+     * This method is partially redundant and is for making sure each sub-class will override it
+     * and implement it differently according to their abilities.
+     */
     @Override
-    public void castAbility() {
+    public void castAbility() {}
 
-    }
-
+    /**
+     * This method is part of the combat system of the game, it is used to attack another unit (primarily enemies)
+     * @param target The unit to attack
+     */
     @Override
     public void dealDamage(Unit target) {
         super.dealDamage(target);
         if (target.healthAmount <= 0) {
-            experience += ((Enemy) target).experienceValue;
+            experience += ((Enemy) target).getExperienceValue();
             Position newPos = target.pos; //TODO: Make sure it updates the currentPosition properly in board
-            ((Enemy)target).death();
+            target.death();
             Tile middleman = b.currentPosition[newPos.x][newPos.y];
             b.currentPosition[newPos.x][newPos.y] = this;
             b.currentPosition[pos.x][pos.y] = middleman;
@@ -45,11 +51,19 @@ public class Player extends Unit implements HeroicUnit{
         }
     }
 
+    /**
+     * This method describes the death of the player - simply calling the method gameOverLose() in our singleton board - in order to finish the game
+     */
     @Override
     public void death() {
-        //b.gameOver(); //TODO: Implement gameOver() method in Board
+        b.gameOverLose();
     }
 
+    /**
+     * This method dictates if the player has enough resources to use his ability
+     * because this method is for a player - which should not be instanced at all, it always returns true
+     * @return true
+     * */
     protected boolean enoughResources() {
         return true;
     }
