81423df473b4fd91babc5823801eca9d0a6597a6
Bershco <hersco@post.bgu.ac.il>
2022-06-15 18:13:34 +0300

    Added javadoc

--- OOPPrinciples_Assignment3/src/Unit.java
+++ OOPPrinciples_Assignment3/src/Unit.java
@@ -9,6 +9,11 @@ public class Unit extends Tile{
         return name;
     }
 
+    /**
+     * This method returns full information of the current unit.
+     * Mainly used to print the information of each unit during combat / on player's turn
+     * @return
+     */
     public String description() {
         throw new UnsupportedOperationException("No implementation yet");
     }
@@ -17,12 +22,27 @@ public class Unit extends Tile{
         return super.toString();
     }
 
+    /**
+     * This method moves a unit
+     * @param d the direction to attempt movement towards
+     */
     public void move(Direction d) {
     }
+
+    /**
+     * This method describes a unit's play when the turn is theirs
+     */
     public void onGameTick() {}
 
+    /**
+     * This method describes a death of a unit
+     */
     public void death() {}
 
+    /**
+     * This method is part of the combat system of the game, it is used to attack another unit
+     * @param target The unit to attack
+     */
     public void dealDamage(Unit target) {
         int attackRoll = (int)Math.round(Math.random()*(attackPoints+1));
         int defenseRoll = (int)Math.round(Math.random()*(target.defensePoints+1));
